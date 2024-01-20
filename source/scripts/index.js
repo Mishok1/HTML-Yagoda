8c91a158af715f72f60ba8f9de695e706c2ceb65
Mishok1 <mm21112000@gmail.com>
2024-01-20 20:52:42 +0300

    Added all styles to mobile

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,18 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.navigation');
+const button = document.querySelector('.header__button');
+
+button.classList.remove('header__button--no-js');
+
+button.addEventListener('click', () => {
+  if (nav.classList.contains('navigation--closed')) {
+    nav.classList.remove('navigation--closed');
+    nav.classList.add('navigation--opened');
+    button.classList.remove('header__button--closed');
+    button.classList.add('header__button--opened');
+  } else {
+    nav.classList.add('navigation--closed');
+    nav.classList.remove('navigation--opened');
+    button.classList.add('header__button--closed');
+    button.classList.remove('header__button--opened');
+  }
+});
